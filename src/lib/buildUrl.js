afed6a4b2b80bd54684a16fe1641a7364778a591
Aleksandra Marinovic <63455453+saska0799@users.noreply.github.com>
2023-03-07 15:48:18 +0100

    Delete buildUrl.js

--- src/lib/buildUrl.js
+++
@@ -1,6 +0,0 @@
-const WEATHER_URL = process.env.REACT_APP_WEATHER_APP_URL;
-const WEATHER_TOKEN = process.env.REACT_APP_WEATHER_APP_TOKEN;
-
-export const buildUrl = (prognosis, city) => {
-  return `${WEATHER_URL}/data/2.5/${prognosis}?q=${city}&appid=${WEATHER_TOKEN}&units=metric`;
-};
